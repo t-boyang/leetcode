bb090357958235f16e2d9fb9caa8392e58f9921c
t-boyang <ilicix@outlook.com>
2021-01-21 17:54:38 +0800

    1/21

---
+++ LargestRectangleArea.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class LargestRectangleArea {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int val = new LargestRectangleArea().largestRectangleArea(new int[] { 2, 1, 5, 6, 2, 3 });
+        System.out.println(val);
+    }
+}

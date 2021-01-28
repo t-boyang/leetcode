4e15841e11f31e5177f4a1e043d4ebd66ddae927
t-boyang <ilicix@outlook.com>
2021-01-28 22:27:14 +0800

    1/28

---
+++ temp/GetLeastNumbers.java
@@ -0,0 +1,99 @@
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+
+public class GetLeastNumbers {
+    public int findKthLargest(int[] nums, int k) {
+        int heapLength = nums.length;
+        buildHeap(nums, heapLength);
+        for (int i = nums.length - 1; i >= nums.length - k + 1; i--) {
+            swap(nums, 0, i);
+            heapLength--;
+            maxHeapify(nums, heapLength, 0);
+        }
+        return nums[0];
+    }
+
+    private void buildHeap(int[] arr, int heapLength) {
+        for (int i = heapLength / 2; i >= 0; i--) {
+            maxHeapify(arr, heapLength, i);
+        }
+    }
+
+    private void maxHeapify(int[] arr, int heapLength, int i) {
+        int pos = i;
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+        if (left < heapLength && arr[left] > arr[pos]) {
+            pos = left;
+        }
+        if (right < heapLength && arr[right] > arr[pos]) {
+            pos = right;
+        }
+        if (i != pos) {
+            swap(arr, i, pos);
+            maxHeapify(arr, heapLength, pos);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // public int[] getLeastNumbers(int[] arr, int k) {
+    // quickSelect(arr, k, 0, arr.length - 1);
+    // int[] ans = new int[k];
+    // for (int i = 0; i < k; i++) {
+    // ans[i] = arr[i];
+    // }
+    // return ans;
+    // }
+
+    // private int quickSelect(int[] arr, int k, int start, int end) {
+    // if (start == end) {
+    // return arr[start];
+    // }
+    // Random random = new Random();
+    // int pivot_index = random.nextInt(end - start) + start;
+    // pivot_index = partition(arr, start, end, pivot_index);
+    // if (pivot_index < k) {
+    // return quickSelect(arr, k, pivot_index + 1, end);
+    // } else if (pivot_index > k) {
+    // return quickSelect(arr, k, start, pivot_index - 1);
+    // } else {
+    // return arr[pivot_index];
+    // }
+    // }
+
+    // private int partition(int[] arr, int start, int end, int pivot_index) {
+    // int pivot = arr[pivot_index];
+    // swap(arr, pivot_index, end);
+    // int store_index = start;
+    // for (int i = start; i < end; i++) {
+    // if (arr[i] < pivot) {
+    // swap(arr, i, store_index);
+    // store_index++;
+    // }
+    // }
+    // swap(arr, store_index, end);
+    // return store_index;
+    // }
+
+    public int[] getLeastNumbers(int[] arr, int k) {
+        Queue<Integer> queue = new PriorityQueue<>((a, b) -> (b - a));
+        for (int i = 0; i < arr.length; i++) {
+            queue.offer(arr[i]);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll();
+        }
+        return ans;
+    }
+}
--- temp/Intersect.java
+++ temp/Intersect.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Intersect {
     public int kthSmallest(int[][] matrix, int k) {
@@ -57,6 +60,7 @@ public class Intersect {
         }
         return Arrays.copyOfRange(ans, 0, k);
     }
+    
 
     public int getSum(int a, int b) {
         while (b != 0) {
@@ -67,8 +71,52 @@ public class Intersect {
         return a;
     }
 
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, nums.length - k, 0, nums.length - 1);
+    }
+
+    private int quickSelect(int[] nums, int k, int start, int end) {
+        if (start >= end) {
+            return nums[start];
+        }
+        Random random = new Random();
+        int pivot_index = random.nextInt(end - start) + start;
+        pivot_index = partion(nums, start, end, pivot_index);
+        if (k < pivot_index) {
+            return quickSelect(nums, k, start, pivot_index - 1);
+        } else if (k > pivot_index) {
+            return quickSelect(nums, k, pivot_index + 1, end);
+        } else {
+            return nums[pivot_index];
+        }
+    }
+
+    private int partion(int[] data, int left, int right, int pivot_index) {
+        int pivot = data[pivot_index];
+        swap(data, pivot_index, right);
+        int store_index = left;
+        for (int i = left; i < right; i++) {
+            if (data[i] < pivot) {
+                swap(data, i, store_index);
+                store_index++;
+            }
+        }
+        swap(data, store_index, right);
+        return store_index;
+    }
+
+    private void swap(int[] data, int left, int right) {
+        int temp = data[left];
+        data[left] = data[right];
+        data[right] = temp;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Intersect().getSum(3, 5));
+        List<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+        // System.out.println(new Intersect().getSum(3, 5));
         // int[] a={1,2,2,1};
         // int[] b={2,2};
         // System.out.println(Arrays.toString(new Intersect().intersect(a, b)));
